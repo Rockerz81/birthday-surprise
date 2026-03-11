a8c1ecf0a85082ccf468633e7849f76757b9d83b
Rockerz81 <gcyber039@gmail.com>
2026-03-11 08:21:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const swiper = new Swiper('.swiper', {
+
+    direction: 'horizontal',
+    loop: false
+
+});
+
+
+function startSite() {
+
+    document.getElementById("popup").style.display = "none";
+
+    document.getElementById("music").play();
+
+    startHearts();
+
+}
+
+
+function startHearts() {
+
+    setInterval(() => {
+
+        const heart = document.createElement("div");
+
+        heart.className = "heart";
+
+        heart.innerHTML = "❤️";
+
+        heart.style.left = Math.random() * 100 + "vw";
+
+        heart.style.fontSize = (20 + Math.random() * 30) + "px";
+
+        document.getElementById("hearts").appendChild(heart);
+
+        setTimeout(() => heart.remove(), 6000);
+
+    }, 500);
+
+}
+
+
+function confetti() {
+
+    confetti({
+
+        particleCount: 200,
+        spread: 120
+
+    });
+
+}
